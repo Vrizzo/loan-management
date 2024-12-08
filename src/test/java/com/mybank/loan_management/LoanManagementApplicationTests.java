5a11339360099a98b30cc123e47af8e8970c293b
Vittorio Rizzo <rizzo.vittorio@gmail.com>
2024-12-08 10:42:20 +0100

    refactor

--- src/test/java/com/mybank/loan_management/LoanManagementApplicationTests.java
+++ src/test/java/com/mybank/loan_management/LoanManagementApplicationTests.java
@@ -4,7 +4,6 @@ import com.mybank.loan_management.dto.LoanRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -22,9 +21,6 @@ class LoanManagementApplicationTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Autowired
-    private Controller controller;
-
     @LocalServerPort
     private int port;
 
@@ -38,9 +34,6 @@ class LoanManagementApplicationTests {
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<LoanRequest> request =
                 new HttpEntity<>(new LoanRequest(), headers);
-        assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/api/v1/loan", request.toString(),  String.class)).contains("Hello, World");
-        ResponseEntity<String> actual = this.restTemplate.postForEntity("http://localhost:" + port + "/api/v1/loan", request.toString(),
-                String.class);
-        assertThat(actual).toString().contains("Hello, World");
+        assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/api/v1/loan", request.toString(), String.class)).contains("Hello, World");
     }
 }
